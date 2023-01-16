9356a90c4ad8ebdd31326506ffbcb7d8b79e83e4
LaurinUB <Laurinub@yahoo.de>
2023-01-16 16:01:00 +0100

    token processing

--- src/lexer.c
+++ src/lexer.c
@@ -18,10 +18,10 @@ t_token	*get_token(char *charset)
 		return (get_io(charset));
 	else
 	{
-		ft_putendl_fd(charset, 1);
 		while (!is_whitespace(charset[i]))
 			i++;
-		return (new_token(ft_substr(charset, 0, i), WORD));
+		if (is_word(charset, i))
+			return (new_token(ft_substr(charset, 0, i), WORD));
 	}
 	return (NULL);
 }
@@ -43,17 +43,12 @@ t_token	**lexer(char *line)
 		if (tokens[j])
 		{
 			ft_putendl_fd(tokens[j]->value, 1);
+			i += ft_strlen(tokens[j]->value);
 			j++;
-			i += ft_strlen(tokens[j]->value) - 1;
+			continue;
 		}
 		i++;
 	}
 	tokens[j] = NULL;
-	//j = 0;
-	//while (tokens[j])
-	//{
-	//	ft_putendl_fd(tokens[j]->value, 1);
-	//	j++;
-	//}
 	return (tokens);
 }
--- src/util.c
+++ src/util.c
@@ -7,15 +7,15 @@ size_t	getnext_whitespace(char *str)
 	i = -1;
 	while (str[++i] != '\0')
 	{
-		if (is_whitespace(str[i]))
-			return (i);
+		if (is_whitespace(str[i + 1]) || str[i + 1] == '\0')
+			return (i + 1);
 	}
 	return (0);
 }
 
 int	is_whitespace(char c)
 {
-	if (ft_strchr(" \t\r\v\f", c))
+	if (ft_strchr(" \t\r\v\f\n", c))
 		return (1);
 	return (0);
 }
