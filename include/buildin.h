7972c0fed386d51ee6f85d6684a1dafe32628ca2
fschmid <fschmid@student.42heilbronn.de>
2023-01-18 15:04:55 +0100

    ft_export

--- include/buildin.h
+++ include/buildin.h
@@ -5,6 +5,7 @@ typedef struct s_options t_options;
 void	ft_pwd(t_options *o);
 void	ft_env(t_options *o);
 void	ft_unset(t_options *o, char *name);
+void	ft_export(t_options *o, char *arg);
 void	ft_cd(t_options *o, char *arg);
 
 
--- src/buildin.c
+++ src/buildin.c
@@ -38,6 +38,28 @@ void	ft_unset(t_options *o, char *name)
 			j++;
 		}
 	}
+	env[j] = NULL;
+	free(o->env);
+	o->env = env;
+}
+
+void	ft_export(t_options *o, char *arg)
+{
+	int		i;
+	int		size;
+	char	**env;
+
+	size = 0;
+	while (o->env[size] != NULL)
+		size++;
+	env = malloc(sizeof(char *) * (size + 2));
+	i = -1;
+	while (o->env[++i] != NULL)
+	{
+		env[i] = o->env[i];
+	}
+	env[i] = ft_strdup(arg);
+	env[i + 1] = NULL;
 	free(o->env);
 	o->env = env;
 }
